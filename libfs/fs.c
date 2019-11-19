efe65122b8bddbcf0d35fa04a37937d35069758e
zackmcd <zackmcd@ad3.ucdavis.edu@pc21.cs.ucdavis.edu>
2019-11-19 12:57:48 -0800

    mount

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,16 +7,93 @@
 #include "disk.h"
 #include "fs.h"
 
-/* TODO: Phase 1 */
+typedef struct __attribute__ ((__packed__)) SuperBlock
+{
+  char sig[8];
+  uint16_t totBlocks;
+  uint16_t rootIndex;
+  uint16_t dataStartIndex;
+  uint16_t totDataBlocks;
+  uint8_t numFATBlocks
+  char padding[4079];
+}SuperBlock, superB_t;
+
+typedef struct __attribute__ ((__packed__)) FATBlock
+{
+  uint16_t word;
+}FATBlock, *fatB_t;
+
+typedef struct __attribute__ ((__packed__)) FAT
+{
+  fatB_t blocks;
+}FAT, FAT_t;
+
+typedef struct __attribute__((__packed__)) Root
+{
+  char name[FS_FILENAME_LEN];
+  uint32_t size;
+  uint16_t firstIndex;
+  char padding[10];
+}Root, *root_t;
+
+typedef struct FDTable
+{
+  int index;
+  int offset;
+}FDTable, *fdt_t;
+
+superB_t superBlock;
+FAT_t fat;
+root_t rootDir = (root_t) malloc(sizeof(Root) * FS_FILE_MAX_COUNT);
+fdt_t fdt = (fdt_t) malloc(sizeof(FDTable) * FS_OPEN_MAX_COUNT);
+
 
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
+  if (block_disk_open(diskname) == -1)
+    return -1;
+ 
+  if (!fat.blocks)
+    return -1;
+
+  for(int i = 0; i < FS_OPENMAX_COUNT;i++)
+  {
+    fdt[i].index = -1;
+    fdt[i].offset = 0;
+  }
+
+  char givenSig[8] = "ECS150FS";
+  
+  if (block_read(0, (void *)&superBlock) == -1)
+    return -1;
+
+  if (strcmp(givenSig, superBlock.sig) != 0)
+    return -1;
+
+  if (block_disk_count() != superBlock.totBlocks)
+    return -1;
+
+  fat.blocks = (fatB_t) malloc(sizeof(FATBlock) * BLOCK_SIZE);
+ 
+  int count = 0;
+  for (int i = 1; i < superBlock.numFATBlocks; i++)
+  {
+    if (block_read(i, (void*)&fat.blocks[(BLOCK_SIZE/2) * count]) == -1)
+      return -1;
+    count++;
+  } 
+
+  if (block_read(superBlock.rootIndex, (void*)&rootDir) == -1)
+    return -1;
+
+  return 0;
+
 }
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
+  
+  free(fat.blocks);
 }
 
 int fs_info(void)
