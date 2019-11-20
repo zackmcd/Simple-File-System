6aace406a7784b4d9c5fb7329ad43755e18b1ccf
sammirowen <40327084+sammirowen@users.noreply.github.com>
2019-11-19 20:30:58 -0800

    Makefile and umount, create and delete started

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,6 +7,8 @@
 #include "disk.h"
 #include "fs.h"
 
+int findFileInRootDirec(const char *filename);
+
 typedef struct __attribute__ ((__packed__)) SuperBlock
 {
   char sig[8];
@@ -34,19 +36,18 @@ typedef struct __attribute__((__packed__)) Root
   uint32_t size;
   uint16_t firstIndex;
   char padding[10];
-}Root, root_t; //*root_t
+}Root, *root_t;
 
 typedef struct FDTable
 {
   int index;
   int offset;
-}FDTable, fdt_t; //*fdt_t
+}FDTable, *fdt_t;
 
 superB_t superBlock;
 FAT_t fat;
-root_t rootDir[FS_FILE_MAX_COUNT];// = (root_t) malloc(sizeof(Root) * FS_FILE_MAX_COUNT);
-fdt_t fdt[FS_OPEN_MAX_COUNT];// = (fdt_t) malloc(sizeof(FDTable) * FS_OPEN_MAX_COUNT);
-
+root_t rootDir = (root_t)malloc(sizeof(Root) * FS_FILE_MAX_COUNT);
+fdt_t fdt = (fdt_t)malloc(sizeof(FDTable) * FS_OPEN_MAX_COUNT);
 
 int fs_mount(const char *diskname)
 {
@@ -73,10 +74,10 @@ int fs_mount(const char *diskname)
   if (block_disk_count() != superBlock.totBlocks)
     return -1;
 
-  fat.blocks = (fatB_t) malloc(sizeof(FATBlock) * BLOCK_SIZE);
+  fat.blocks = (fatB_t)malloc(sizeof(FATBlock) * BLOCK_SIZE);
  
   int count = 0;
-  for (int i = 1; i < superBlock.numFATBlocks; i++)
+  for (int i = 1; i <= superBlock.numFATBlocks; i++)
   {
     if (block_read(i, (void*)&fat.blocks[(BLOCK_SIZE/2) * count]) == -1)
       return -1;
@@ -87,72 +88,143 @@ int fs_mount(const char *diskname)
     return -1;
 
   return 0;
-
 }
 
 int fs_umount(void)
 {
-  
+  //chck if still open file descriptors
+  for(int i = 0; i < FS_OPEN_MAX_COUNT; i++)
+  {
+    //if a files index is not -1, the file is open 
+    if(fdt[i].index != -1)
+      return -1;
+  }
+
+  //write root directory out to disk
+  if(block_write(superBlock.rootIndex, (void*)&rootDir) == -1)
+    return -1;
+
+  //write blocks out to disk
+  int count = 0;
+  for(int i = 1; i <= superBlock.numFATBlocks; i++)
+  {
+    if(block_write(i, (void*)&fat.blocks[(BLOCK_SIZE/2) * count]))
+      return -1;
+    count++;
+  }
+
+  //check disk can be closed
+  if(block_disk_close() == -1)
+    return -1;
+
+  free(rootDir);
   free(fat.blocks);
+
   return 0;
 }
 
 int fs_info(void)
 {
   return 0;
-	/* TODO: Phase 1 */
+}
+
+int findFileInRootDirec(const char *filename)
+{
+  for(int i = 0; i < FS_FILE_MAX_COUNT; i++)
+  {
+    if(strcmp(rootDir[i].name, filename) == 0)
+      return i; 
+  }
+  return -1;
 }
 
 int fs_create(const char *filename)
 {
+  //if filename is invalid
+  if(filename == NULL)
+    return -1;
+
+  //if length of filename is too long
+  if(strlen(filename) > FS_FILENAME_LEN)
+    return -1;
+
+  //if file name already exists in file directory
+  if(findFileInRootDirec(filename) == -1)
+    return -1;
+
+  //find empty entry in root directory
+  for(int i = 0; i < FS_FILE_MAX_COUNT; i++)
+  {
+    if(rootDir[i].name == NULL){
+      strcpy(rootDir[i].name, filename);
+      rootDir[i].size = 0;
+      rootDir[i].firstIndex = FAT_EOC; 
+  }
+
+  //if root directory is already full
+  if(i == FS_FILE_MAX_COUNT - 1)
+    return -1;
+
   return 0;
-	/* TODO: Phase 2 */
 }
 
 int fs_delete(const char *filename)
 {
+  //filename is invalid
+  if(filename == NULL)
+    return -1;
+
+  //no file in root directory to delete
+  int check = findFileInRootDirec(filename);
+  if(check == -1)
+    return -1;	  
+  else 
+  {
+    //check if file is open
+   
+  } 
   return 0;
-	/* TODO: Phase 2 */
 }
 
 int fs_ls(void)
 {
-  return 0;
 	/* TODO: Phase 2 */
+  return 0;
 }
 
 int fs_open(const char *filename)
 {
-  return 0;
 	/* TODO: Phase 3 */
+  return 0;
 }
 
 int fs_close(int fd)
 {
-  return 0;
 	/* TODO: Phase 3 */
+  return 0;
 }
 
 int fs_stat(int fd)
 {
-  return 0;
 	/* TODO: Phase 3 */
+  return 0;
 }
 
 int fs_lseek(int fd, size_t offset)
 {
-  return 0;
 	/* TODO: Phase 3 */
+  return 0;
 }
 
 int fs_write(int fd, void *buf, size_t count)
 {
-  return 0;
 	/* TODO: Phase 4 */
+  return 0;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
-  return 0;
 	/* TODO: Phase 4 */
+  return 0;
 }
+
